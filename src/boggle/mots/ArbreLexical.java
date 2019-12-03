5db8bb9ac6365822be1e11c4fa0239c8dbb8d1d2
Delgrange Pierre <pierredelgrange@hotmail.fr>
2019-12-03 22:53:56 +0100

    debut arbre

--- src/boggle/mots/ArbreLexical.java
+++ src/boggle/mots/ArbreLexical.java
@@ -1,5 +1,6 @@
 package boggle.mots;
 
+import java.util.Arrays;
 import java.util.List ;
 
 /** La classe ArbreLexical permet de stocker de façon compacte et
@@ -8,10 +9,20 @@ public class ArbreLexical {
     public static final int TAILLE_ALPHABET = 26 ;
     private boolean estMot ; // vrai si le noeud courant est la fin d'un mot valide
     private ArbreLexical[] fils = new ArbreLexical[TAILLE_ALPHABET] ; // les sous-arbres
+    private char caractere;
     
     /** Crée un arbre vide (sans aucun mot) */
     public ArbreLexical() {
-        // à compléter
+        this.caractere = ' ';
+        //On initialise le tableau d'arbre fils avec toute les lettres de l'alphabet
+        char c1 = 'a';
+        for(int i = 0;i<fils.length;i++){
+            fils[i] = new ArbreLexical(c1);
+            c1++;
+        }
+    }
+    public ArbreLexical(char caractere){
+        this.caractere = caractere;
     }
 
     /** Indique si le noeud courant est situé à l'extrémité d'un mot
@@ -23,8 +34,56 @@ public class ArbreLexical {
      * @return <code>true</code> si le mot a été ajouté,
      * <code>false</code> sinon*/
     public boolean ajouter(String word) {
-        // à compléter
-        return false ;
+        //Si le mot est une seule lettre on change le booleen à true
+        if(word.length() == 1){
+            this.estMot = true;
+            return true;
+        }
+        //On récupere la premiere lettre du mot
+        char premiereLettre = word.charAt(0);
+
+        //On recupere l'indice de la lettre dans le tableau d'arbre fils
+        // -1 si la lettre n'est pas présente
+        int indiceFils = lettreContenuDansFils(premiereLettre);
+
+        if(indiceFils == -1){
+            //le tableau fils est null on crée un nouveau tableau
+            if(this.fils == null){
+                this.fils = new ArbreLexical[1];
+            }else {
+                //Sinon on agrandi le tableau existant
+                this.fils = agrandiTab(this.fils);
+            }
+            //on ajoute la lettre au premier membre
+            this.fils[0] = new ArbreLexical(premiereLettre);
+            indiceFils = 0;
+        }
+        //On ajoute la fin du mot sur l'arbre au bon indice
+        return this.fils[indiceFils].ajouter(word.substring(1));
+    }
+
+
+    //Méthode qui permet de retourner l'indice dans le tableau des fils
+    // où se trouve la lettre donnée en parametre
+    private int lettreContenuDansFils (char c){
+        if(this.fils != null) {
+            for (int i = 0; i < fils.length; i++) {
+                if (fils[i] != null && fils[i].caractere == c) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    //Méthode permettant d'agrandir le tableau en laissant libre le premier élément
+    private ArbreLexical [] agrandiTab(ArbreLexical [] tab){
+        ArbreLexical [] resultat;
+        resultat = new ArbreLexical[tab.length + 1];
+        for(int i = 0, j = 1 ; i < tab.length ; i++ , j++){
+            resultat[j] = tab[i];
+        }
+        return resultat;
     }
 
     /** Teste si l'arbre lexical contient le mot spécifié.
@@ -51,5 +110,30 @@ public class ArbreLexical {
     public static ArbreLexical lireMots(String fichier) {
         // à compléter
         return null ;
+    }
+    private boolean estUneFeuille(){
+        return this.fils == null || this.fils.length == 0;
+    }
+    public String toString(){
+
+        if(this.estUneFeuille()){
+           return "" + this.caractere;
+       }
+
+       String resultat = "";
+       for(ArbreLexical arbreLexical : fils){
+           if (arbreLexical != null) {
+               resultat += arbreLexical.toString();
+           }
+       }
+       return resultat;
+    }
+
+    public static void main(String[] args) {
+        ArbreLexical arbreLexical = new ArbreLexical();
+        System.out.println(arbreLexical);
+        arbreLexical.ajouter("marche");
+        System.out.println(arbreLexical);
+
     }
 }
